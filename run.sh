04dfa7f08a06ea9279b791c05267e81d8307890b
giri <giridharpn@gmail.com>
2017-01-18 13:35:30 +0000

    Included APItest

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 mvn clean test -Dfileupload=https://upload.publishing.discovery.onsdigital.co.uk -Dfilename=ReMapped-AF001EW.csv -Dtest=**/APIIntegrityTest.java
+mvn clean test -Dtest=**/APITest.java
