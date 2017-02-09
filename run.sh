c812205be116cd41c063c7dd7a6b150c6e3e7e89
giri <giridharpn@gmail.com>
2017-02-09 12:54:14 +0000

    Changed the file- isolated file for E2E tests

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mvn clean test -Dfileupload=https://upload.publishing.discovery.onsdigital.co.uk -Dfilename=AF001EW-sample.csv -Dtest=**/APIIntegrityTest.java -Dbackend=real
+mvn clean test -Dfileupload=https://upload.publishing.discovery.onsdigital.co.uk -Dfilename=Open-Data-v3_E2E_Tests.csv -Dtest=**/APIIntegrityTest.java -Dbackend=real
 mvn test -Dtest=**/APITest.java -Dbackend=real
 mvn test -Dtest=**/CSVFilterTest.java -Djobcreator=https://discovery.onsdigital.co.uk/dd/api/jobs -Dbackend=real
 mvn test -Dtest=**/ValidateAPIStubTest.java
