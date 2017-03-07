73b4045ffe4ac3ab365b78f0e2d75c294a34c043
giri <giridharpn@gmail.com>
2017-03-07 10:33:43 +0000

    to run the tests locally

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-mvn clean test -Dtest=**/APIIntegrityTest.java
+mvn clean test -Dtest=**/APIIntegrityTest.java -Dfilename=Open-Data-v3_E2E_Tests.csv
+mvn clean test -Dtest=**/APIIntegrityTest.java -Dfilename=AF001EW_v3_E2E_Tests.csv
+mvn test -Dtest=**/CSVFilterTest.java -Dfilename=Open-Data-v3_E2E_Tests.csv
+mvn clean test -Dtest=**/APIIntegrityTest.java -Dfilename=AF001EW_v3_E2E_Tests.csv
 #mvn test -Dtest=**/APITest.java -Dbackend=real
-mvn test -Dtest=**/CSVFilterTest.java
+
 #mvn test -Dtest=**/MetaDataEditorTest.java -Dfilename=Open-Data-v3_E2E_Tests.csv
 #mvn test -Dtest=**/ValidateAPIStubTest.java
 
