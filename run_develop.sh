f98e084704cfe8ef7c53ed8b649aa10eef42b7bd
giri <giridharpn@gmail.com>
2017-03-08 13:02:20 +0000

    changes to make it run local

--- run_develop.sh
+++ run_develop.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 mvn clean test -Dtest=**/APIIntegrityTest.java -Dfilename=Open-Data-v3_E2E_Tests.csv
-mvn clean test -Dtest=**/APIIntegrityTest.java -Dfilename=AF001EW_v3_E2E_Tests.csv
+mvn test -Dtest=**/APIIntegrityTest.java -Dfilename=AF001EW_v3_E2E_Tests.csv
 mvn test -Dtest=**/CSVFilterTest.java -Dfilename=Open-Data-v3_E2E_Tests.csv
-mvn clean test -Dtest=**/CSVFilterTest.java -Dfilename=AF001EW_v3_E2E_Tests.csv
+mvn test -Dtest=**/CSVFilterTest.java -Dfilename=AF001EW_v3_E2E_Tests.csv
 
 
 
